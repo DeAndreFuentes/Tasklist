f20c7bfc42b286a055ded21e0825a4d0fc1f2478
DeAndreFuentes <74945613+DeAndreFuentes@users.noreply.github.com>
2021-01-22 00:47:54 -0500

    Add files via upload

---
+++ taskList/style/Javascript/task.js
@@ -0,0 +1,10 @@
+class Task{
+    constructor( id, title, startDate, dueDate,  discription, status){
+        this.id = id;
+        this.title = title;
+        this.startDate = startDate;
+        this.dueDate = dueDate;
+        this.discription = discription;
+        this.status = status;
+    }
+}
